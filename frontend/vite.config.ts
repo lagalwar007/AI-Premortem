f1bf2c5adc2b4da16b6f5afe142ded1af655163a
Karan <lagalwar007@gmail.com>
2026-07-19 14:30:23 +0530

    add: config changes for build error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { API_URL, API_PORT } from "./src/config";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            "/premortem": `${API_URL}:${API_PORT}`,
-        },
-    },
 });
